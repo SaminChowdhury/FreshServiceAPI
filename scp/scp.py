4b8c1a0686e190314fbed9eaadb6f5d5dc523e98
Samin Chowdhury <119480295+SaminChowdhury@users.noreply.github.com>
2023-06-27 12:10:47 -0400

    Delete scp directory

--- scp/scp.py
+++
@@ -1,8 +0,0 @@
-import os 
-while True:
-    try:
-        os.popen('sh /home/freshservice/ClosedTickets/scp.sh')
-        print("Sent To Grafana")
-    except:
-        time.sleep(1)
-        print("Could not connect to Grafana")
