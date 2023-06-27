b3a10b88a7ff8ac3c7d3a01b6fbb3e8a2d8b183d
Samin Chowdhury <119480295+SaminChowdhury@users.noreply.github.com>
2023-06-27 12:11:10 -0400

    Update secure.py

--- ClosedTickets/secure.py
+++ ClosedTickets/secure.py
@@ -1,3 +1,3 @@
-API_KEY = "QrIY0ibVgAMElv5tdLOe"   # Enter API KEY
+API_KEY = ""   # Enter API KEY
 GROUP_ID = "15000283771" # Enter Group ID
 DOMAIN = "https://keanuniversity.freshservice.com"     # Enter Domain URL
